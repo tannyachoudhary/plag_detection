35ad1cd7790311f73d051caa74727c4fcb0336ab
tannyachoudhary <tannyachoudhary700@gmail.com>
2026-04-20 21:42:21 +0530

    Update tailwind.config.js
    asdfgh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-export default {
+export default
+{
   darkMode: ["class"],
   content: [
     './client/**/*.{js,jsx,html}',
   ],
-  theme: {
-    extend: {
-      borderRadius: {
+  theme: 
+  {
+    extend: 
+    {
+      borderRadius: 
+      {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
